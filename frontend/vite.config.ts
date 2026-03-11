df2d307732125b5365ba8a8d9f3538a0360f6fbe
molihuaa <yasmineedaoudii@gmail.com>
2026-03-11 04:36:00 +0000

    fix allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true
-  },
   preview: {
+    host: true,
     port: 3000,
-    host: true
+    allowedHosts: ['all']
+  },
+  server: {
+    host: true,
+    port: 5173
   }
 })
